96db33dfd5efa6a0fcad3e348daca849842cb438
Joydeep Bhandari <joydeepbhandari76@gmail.com>
2025-09-22 13:25:42 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -22,7 +22,7 @@ export const AboutSection = () => {
                         <a href="#contact" className="cosmic-button">
                             Get In Touch
                         </a>
-                        <a href="https://drive.google.com/file/d/1aNgym8DY0FN-hubz1dYF-lv5knDakUl9/view?usp=drive_link" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                        <a href="https://drive.google.com/file/d/12OGi-_-Ox-80mOpq4RUatFVzKfgsIOS-/view?usp=sharing" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                             Download CV
                         </a>
                     </div>
@@ -77,5 +77,6 @@ export const AboutSection = () => {
     </section>
 
 }
+
 
 
